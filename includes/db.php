6eafd016026cbe7dba100ca73c338f4cad62a9b7
Peeyush <madhavanpeeyush@gmail.com>
2021-03-22 00:03:52 +0530

    Made PHP pages

---
+++ includes/db.php
@@ -0,0 +1,14 @@
+<?php 
+ 
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "cms";
+
+$connection= mysqli_connect($db_host,$db_user,$db_pass,$db_name);
+if($connection)
+{
+    echo "We are connected!";
+}
+
+?>
