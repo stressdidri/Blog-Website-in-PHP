6ec916c7719159716ccd552c47cdba25ac6b7293
Peeyush <madhavanpeeyush@gmail.com>
2021-03-22 11:14:28 +0530

    Categories Refactoring Done

--- admin/categories.php
+++ admin/categories.php
@@ -1,27 +1,21 @@
 <?php include "includes/admin_header.php" ?>
-
 <div id="wrapper">
   <!-- Navigation -->
 <?php include "includes/admin_navigation.php" ?>
-
-
   <div id="page-wrapper">
     <div class="container-fluid">
       <!-- Page Heading -->
       <div class="row">
         <div class="col-lg-12">
-
-
           <h1 class="page-header">
             Wlcome to Admin: 
             <small>Author   </small>
           </h1>
-
+<!-- START -->
 <div class="col-xs-6">
 <?php 
 insert_categories();
 ?>
-
         <form action="" method="post">
             <div class="form-group">
             <label for="cat_title">Add Category</label>
@@ -32,14 +26,13 @@ insert_categories();
                 <input class="btn btn-primary" type="submit" name="submit" value="Add Category ">
             </div>
         </form>
-<?php 
+
+<?php //Update and Include
 if(isset($_GET['edit']))
 {
   $cat_id = $_GET['edit'];
   include "includes/update_categories.php";
 }
-
-
 ?>
 
 </div>
@@ -52,42 +45,17 @@ if(isset($_GET['edit']))
         </thead>
         <tbody>
 
-<?php  //FIND ALL CATEGORIES
-$query = "SELECT * FROM categories";
-$select_categories = mysqli_query($connection,$query);
+<!-- FIND ALL CATEGORIES -->
+<?php  findAllCategories(); ?>
 
-while($row=mysqli_fetch_assoc($select_categories))
-{
-    $cat_id= $row["cat_id"];
-    $cat_title= $row["cat_title"];
-    echo "<tr>";
-    echo "<td>{$cat_id}</td>";
-    echo "<td>{$cat_title}</td>";
-    echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
-    echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
-    echo "</tr>";
-}
-
-?>
-
-<?php //DELETE QUERY
-
-if(isset($_GET['delete']))
-{
-  $the_cat_id=$_GET['delete'];
-
-  $query= "DELETE FROM categories WHERE cat_id={$the_cat_id}";
-  $delete_query=mysqli_query($connection,$query);
-  header("Location: categories.php");
-}
-
-?>
+<!-- DELETE QUERY -->
+<?php deleteCategories() ?>
 
         </tbody>
     </table>
 </div>
 
-
+<!-- END -->
         </div>
       </div>
       <!-- /.row -->
@@ -97,5 +65,4 @@ if(isset($_GET['delete']))
   <!-- /#page-wrapper -->
 </div>
 <!-- /#wrapper -->
-
 <?php include "includes/admin_footer.php" ?>
