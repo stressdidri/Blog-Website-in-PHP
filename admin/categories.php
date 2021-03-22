727d9fd124fd46709837fcc73246b912bb995439
Peeyush <madhavanpeeyush@gmail.com>
2021-03-22 09:49:17 +0530

    Displaying Data in Category

--- admin/categories.php
+++ admin/categories.php
@@ -1,8 +1,8 @@
-<?php include "includes/header.php" ?>
+<?php include "includes/admin_header.php" ?>
 
 <div id="wrapper">
   <!-- Navigation -->
-<?php include "includes/navigation.php" ?>
+<?php include "includes/admin_navigation.php" ?>
 
 
   <div id="page-wrapper">
@@ -30,16 +30,37 @@
         </form>
 </div>
 <div class="col-xs-6">
+
+<?php 
+
+$query = "SELECT * FROM categories";
+$select_categories = mysqli_query($connection,$query);
+
+?>
+
     <table class="table table-bordered table-hover">
         <thead>
             <th>ID</th>
             <th>Category Title</th>
         </thead>
         <tbody>
-            <tr>
-            <td>Baseball Category</td>
-            <td>Baseball Category</td>
-            </tr>
+
+<?php 
+
+while($row=mysqli_fetch_assoc($select_categories))
+{
+    $cat_id= $row["cat_id"];
+    $cat_title= $row["cat_title"];
+    echo "<tr>";
+    echo "<td>{$cat_id}</td>";
+    echo "<td>{$cat_title}</td>";
+    echo "</tr>";
+
+
+}
+
+?>
+
         </tbody>
     </table>
 </div>
@@ -55,4 +76,4 @@
 </div>
 <!-- /#wrapper -->
 
-<?php include "includes/footer.php" ?>
+<?php include "includes/admin_footer.php" ?>
