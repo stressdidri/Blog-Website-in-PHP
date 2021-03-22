c61a77a9f8c72bd4b921a1ccc63e366d2162a30c
Peeyush <madhavanpeeyush@gmail.com>
2021-03-22 10:10:04 +0530

    Adding Categories in DB

--- admin/categories.php
+++ admin/categories.php
@@ -18,7 +18,30 @@
           </h1>
 
 <div class="col-xs-6">
-        <form action="">
+<?php 
+if(isset($_POST["submit"]))
+{
+  $cat_title=$_POST["cat_title"];
+  if($cat_title== "" || empty($cat_title))
+    {
+      echo "This field should not be empty";
+    }
+  else
+    {
+      $query="INSERT INTO categories(cat_title) VALUE('{$cat_title}')";
+      $create_category_query=mysqli_query($connection, $query);
+
+      if(!$create_category_query)
+      {
+        die ("Add Category Failed") . mysqli_error($connection);
+      }
+    }
+
+}
+
+?>
+
+        <form action="" method="post">
             <div class="form-group">
             <label for="cat_title">Add Category</label>
                 <input class="form-control" type="text" name="cat_title" id="">
