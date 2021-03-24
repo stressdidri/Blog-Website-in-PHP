0e5c549e946e543791099c5d927fb8ffdf6c05fe
Peeyush <madhavanpeeyush@gmail.com>
2021-03-24 07:49:23 +0530

    Edit Post Part 3

--- admin/includes/edit_posts.php
+++ admin/includes/edit_posts.php
@@ -35,8 +35,21 @@ while($row=mysqli_fetch_assoc($select_posts_by_id))
      </div>
 
       <div class="form-group">
-      <label for="post_category_id">Post Category Id
-      <input type="text" class="form-control" name="post_category_id" value='<?php echo $post_category_id; ?>'>  
+      <select name="post_category" id="post_category">
+      <?php 
+      $query = "SELECT * FROM categories";
+      $select_categories = mysqli_query($connection,$query);
+      confirmQuery($select_categories);
+      while($row=mysqli_fetch_assoc($select_categories))
+      {
+          $cat_id= $row["cat_id"];
+          $cat_title= $row["cat_title"];
+
+          echo "<option value='$cat_id'>$cat_title</option>";
+      }
+      
+      ?>
+      </select>
      
      </div>
 
@@ -54,8 +67,7 @@ while($row=mysqli_fetch_assoc($select_posts_by_id))
      
      
    <div class="form-group">
-        <label for="post_image">Post Image</label>
-         <input type="file"  name="image">
+   <img width='100' src="../images/<?php echo $post_image; ?>" alt="">
      </div>
 
      <div class="form-group">
@@ -65,8 +77,7 @@ while($row=mysqli_fetch_assoc($select_posts_by_id))
      
      <div class="form-group">
         <label for="post_content">Post Content</label>
-        <textarea class="form-control "name="post_content" id="body" cols="30" rows="10">
-        <?php echo $post_content; ?>
+        <textarea class="form-control "name="post_content" id="body" cols="30" rows="10"><?php echo $post_content; ?>
         </textarea>
      </div>
 
