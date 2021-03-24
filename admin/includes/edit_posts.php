9bf6fa7f5e608d6a2cc33f37e5420decc733ca3b
Peeyush <madhavanpeeyush@gmail.com>
2021-03-24 07:32:46 +0530

    Edit Post Part 2

--- admin/includes/edit_posts.php
+++ admin/includes/edit_posts.php
@@ -1,26 +1,54 @@
+<?php 
+if(isset($_GET['p_id']))
+{
+$the_post_id=$_GET['p_id'];
+}
+
+$query = "SELECT * FROM posts";
+$select_posts_by_id = mysqli_query($connection,$query);
+
+while($row=mysqli_fetch_assoc($select_posts_by_id))
+{
+    $post_id= $row["post_id"];
+    $post_author= $row["post_author"];
+    $post_title= $row["post_title"];
+    $post_category_id= $row["post_category_id"];
+    $post_status= $row["post_status"];
+    $post_image= $row["post_image"];
+    $post_content= $row["post_content"];
+    $post_tags= $row["post_tags"];
+    $post_comment_count= $row["post_comment_count"];
+    $post_date= $row["post_date"];
+}
+
+?>
+
+
+
+
 <form action="" method="post" enctype="multipart/form-data">    
      
      
      <div class="form-group">
         <label for="title">Post Title</label>
-         <input type="text" class="form-control" name="title">
+         <input type="text" class="form-control" name="title" value='<?php echo $post_title; ?>'>
      </div>
 
       <div class="form-group">
       <label for="post_category_id">Post Category Id
-      <input type="text" class="form-control" name="post_category_id">  
+      <input type="text" class="form-control" name="post_category_id" value='<?php echo $post_category_id; ?>'>  
      
      </div>
 
 
       <div class="form-group">
       <label for="author">Post Author</label>
-      <input type="text" class="form-control" name="author">      
+      <input type="text" class="form-control" name="author" value='<?php echo $post_author; ?>' > 
      </div> 
 
       <div class="form-group">
       <label for="post_status">Post Status</label>
-      <input type="text" class="form-control" name="post_status">
+      <input type="text" class="form-control" name="post_status" value='<?php echo $post_status; ?>'>
       </div>
      
      
@@ -32,12 +60,13 @@
 
      <div class="form-group">
         <label for="post_tags">Post Tags</label>
-         <input type="text" class="form-control" name="post_tags">
+         <input type="text" class="form-control" name="post_tags" value='<?php echo $post_tags; ?>'>
      </div>
      
      <div class="form-group">
         <label for="post_content">Post Content</label>
         <textarea class="form-control "name="post_content" id="body" cols="30" rows="10">
+        <?php echo $post_content; ?>
         </textarea>
      </div>
 
--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -13,6 +13,7 @@
                 <th>Tags</th>
                 <th>Comments</th>
                 <th>Date</th>
+                <th>Update</th>
                 <th>Delete</th>
               </tr>
             </thead>
@@ -43,6 +44,7 @@ while($row=mysqli_fetch_assoc($select_posts))
     echo "<td>$post_tags</td>";
     echo "<td>$post_comment_count</td>";
     echo "<td>$post_date</td>";
+    echo "<td><a href='posts.php?source=edit_post&p_id=$post_id'>Edit</a></td>";
     echo "<td><a href='posts.php?delete=$post_id'>Delete</a></td>";
 
 
