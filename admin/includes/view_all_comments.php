dbb007814c66c5fc1329f7e00dc0de29c9aa890c
Peeyush <madhavanpeeyush@gmail.com>
2021-03-24 20:34:26 +0530

    Approving and Unapproving Comments

--- admin/includes/view_all_comments.php
+++ admin/includes/view_all_comments.php
@@ -67,8 +67,8 @@ while($row=mysqli_fetch_assoc($select_comments))
 
     echo "<td>$comment_date</td>";
 
-    echo "<td><a href='posts.php?source=edit_post&p_id='>Approve</a></td>";
-    echo "<td><a href='posts.php?delete='>Unapprove</a></td>";
+    echo "<td><a href='comments.php?approve=$comment_id'>Approve</a></td>";
+    echo "<td><a href='comments.php?unapprove=$comment_id'>Unapprove</a></td>";
 
     echo "<td><a href='comments.php?delete=$comment_id'>Delete</a></td>";
     echo "</tr>";
@@ -78,6 +78,28 @@ while($row=mysqli_fetch_assoc($select_comments))
             </tbody>
           </table>
 <?php 
+
+
+if(isset($_GET['approve']))
+{
+  $the_comment_id=$_GET['approve'];
+  $query = "UPDATE comments SET comment_status = 'approved' WHERE comment_id=$the_comment_id";
+
+  $approve_comment_query=mysqli_query($connection,$query);
+  header ("Location:comments.php");
+}
+
+
+if(isset($_GET['unapprove']))
+{
+  $the_comment_id=$_GET['unapprove'];
+  $query = "UPDATE comments SET comment_status = 'unapproved' WHERE comment_id=$the_comment_id";
+
+  $unapprove_comment_query=mysqli_query($connection,$query);
+  header ("Location:comments.php");
+}
+
+
 if(isset($_GET['delete']))
 {
   $the_comment_id=$_GET['delete'];
