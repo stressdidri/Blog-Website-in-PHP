34327280beca746c38bbda3547ff1aa7b2c8e271
Peeyush <madhavanpeeyush@gmail.com>
2021-03-24 06:54:02 +0530

    Creating Posts Done

--- admin/functions.php
+++ admin/functions.php
@@ -1,5 +1,16 @@
 <?php 
 
+function confirmQuery($result)
+{
+  global $connection;
+  if(!$result)
+  {
+      die("Query Failed".mysqli_error($connection));
+  }
+
+  
+}
+
 function insert_categories()
 {
     global $connection;
--- admin/includes/add_posts.php
+++ admin/includes/add_posts.php
@@ -5,21 +5,27 @@
    if(isset($_POST['create_post'])) {
    
             $post_title = $_POST['title'];
-            $post_user = $_POST['post_user'];
-            $post_category_id = $_POST['post_category'];
+            $post_author = $_POST['author'];
+            $post_category_id = $_POST['post_category_id'];
             $post_status = $_POST['post_status'];
-    
             $post_image = $_FILES['image']['name'];
             $post_image_temp = $_FILES['image']['tmp_name'];
-    
-    
+   
             $post_tags = $_POST['post_tags'];
             $post_content = $_POST['post_content'];
             $post_date = date('d-m-y');
             $post_comment_count=4;
 
-       
         move_uploaded_file($post_image_temp, "../images/$post_image" );
+
+        $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date,post_image,post_content,post_tags,post_status) ";
+
+        $query .= "VALUES({$post_category_id},'{$post_title}','{$post_author}',now(),'{$post_image}','{$post_content}','{$post_tags}', '{$post_status}') ";
+
+        $create_post_query = mysqli_query($connection, $query); 
+
+        confirmQuery($create_post_query);
+
 
    }
     
@@ -36,31 +42,22 @@
           <input type="text" class="form-control" name="title">
       </div>
 
-         <div class="form-group">
-       <label for="category">Category</label>
-       <select name="post_category" id="">    
-        
-       </select>
+       <div class="form-group">
+       <label for="post_category_id">Post Category Id
+       <input type="text" class="form-control" name="post_category_id">  
       
       </div>
 
 
        <div class="form-group">
-       <label for="users">Users</label>
-       <select name="post_user" id="">
-              
-        
-       </select>
-      
+       <label for="author">Post Author</label>
+       <input type="text" class="form-control" name="author">      
       </div> 
 
        <div class="form-group">
-         <select name="post_status" id="">
-             <option value="draft">Post Status</option>
-             <option value="published">Published</option>
-             <option value="draft">Draft</option>
-         </select>
-      </div>
+       <label for="post_status">Post Status</label>
+       <input type="text" class="form-control" name="post_status">
+       </div>
       
       
       
@@ -79,8 +76,6 @@
          <textarea class="form-control "name="post_content" id="body" cols="30" rows="10">
          </textarea>
       </div>
-      
-      
 
        <div class="form-group">
           <input class="btn btn-primary" type="submit" name="create_post" value="Publish Post">
