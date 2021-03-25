9dcf97479a173a134939952f4ce5d5d893aecbe2
Peeyush <madhavanpeeyush@gmail.com>
2021-03-25 18:33:57 +0530

    Validating User Admin

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -4,6 +4,22 @@
 <?php include "../includes/db.php" ?>
 <?php include "functions.php" ?>
 
+
+<?php 
+if(isset($_SESSION['user_role']))
+{
+  if($_SESSION['user_role']!=='Admin')
+  {
+    header("Location:../index.php"); 
+  }
+}
+
+
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
