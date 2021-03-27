b7c3b16a6f68cb0aca579b7684ac6e395e52e241
Peeyush <madhavanpeeyush@gmail.com>
2021-03-27 19:52:29 +0530

    Adding Dropdown to Post Page

--- admin/includes/add_posts.php
+++ admin/includes/add_posts.php
@@ -52,8 +52,12 @@
     </div> 
 
     <div class="form-group">
-       <label for="post_status">Post Status</label>
-       <input type="text" class="form-control" name="post_status">
+       <select name="post_status" id="">
+       <option value="Draft">Post Status</option>
+       <option value="Published">Publish</option>
+       <option value="Draft">Draft</option>
+
+       </select>
     </div>
       
     <div class="form-group">
